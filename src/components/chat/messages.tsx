af7a443b12a6ecc0a792b3a94ac8c12cc0a7d87a
Blanka Semanová <blanka.seman@gmail.com>
2025-07-16 21:44:07 +0200

    feat: Fix pading

--- src/components/chat/messages.tsx
+++ src/components/chat/messages.tsx
@@ -3,7 +3,7 @@ import { MessageItem, MessageItemProps } from "./message-item";
 export const Messages = ({ messages }: { messages: MessageItemProps[] }) => {
   console.log(messages);
   return (
-    <div className="md:mt-20 px-8 size-full flex flex-col gap-4">
+    <div className="md:mt-20 px-4 size-full flex flex-col gap-4">
       {messages.map((message) => (
         <MessageItem
           key={message.content}
